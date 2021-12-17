bc4796df13603214d0ce328dd5116b10b897e0c4
Mark <mark.j.wee@gmail.com>
2021-12-17 12:08:55 -0500

    commit tree d35f28423c578decd2e324b91f343ea72aed21a2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+let employee = {
+    name: "Cookie Wee",
+    streetAddress: "433 Dean St",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee}.birthdate = "March 25, 1997"
+};
+
+//.push(key) = value
